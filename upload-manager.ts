10f9691360052b8c9ab3dbc30b979e73bfef9d6d
Mike Duffey <mpduffey@gmail.com>
2016-10-02 14:10:09 +0000

    mike

---
+++ upload-manager.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'upload-manager',
+	template: `
+		<h1>Upload Manager</h1>
+		<button class="btn btn-primary">Select file</button>
+	`
+})
+
+export class UploadManager {}
